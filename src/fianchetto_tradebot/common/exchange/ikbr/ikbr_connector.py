dbb05c05649105a5f7fb75ac4d3307a8fb11796b
Aleks Shulman <aleksshulman@Alekss-MacBook-Pro.local>
2025-04-17 19:03:42 -0500

    Create rest_service interface Implement cancel order in oex service

--- src/fianchetto_tradebot/common/exchange/ikbr/ikbr_connector.py
+++ src/fianchetto_tradebot/common/exchange/ikbr/ikbr_connector.py
@@ -7,7 +7,7 @@ from fianchetto_tradebot.common.exchange.connector import Connector
 
 config = configparser.ConfigParser()
 
-DEFAULT_CONFIG_FILE = os.path.join(os.path.dirname(__file__), 'config.ini')
+DEFAULT_IKBR_CONFIG_FILE = os.path.join(os.path.dirname(__file__), 'config.ini')
 DEFAULT_IKBR_SESSION_FILE = os.path.join(os.path.dirname(__file__), 'ikbr_session.out')
 
 
@@ -16,7 +16,7 @@ logger = logging.getLogger(__name__)
 
 class IkbrConnector(Connector):
 
-    def __init__(self, config_file=DEFAULT_CONFIG_FILE, session_file=DEFAULT_ETRADE_SESSION_FILE):
+    def __init__(self, config_file=DEFAULT_IKBR_CONFIG_FILE, session_file=DEFAULT_ETRADE_SESSION_FILE):
         self.config_file = config_file
         self.session_file = session_file
 
