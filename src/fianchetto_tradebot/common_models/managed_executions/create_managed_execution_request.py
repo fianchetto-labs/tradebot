50e9c831196993c39e449faed093e42c640ff2a9
Aleks Shulman <aleksshulman@Alekss-MacBook-Pro.local>
2025-06-10 11:44:43 -0500

    FIA-118: Add testing and fix for class that has ManagedExecution as a member

--- src/fianchetto_tradebot/common_models/managed_executions/create_managed_execution_request.py
+++ src/fianchetto_tradebot/common_models/managed_executions/create_managed_execution_request.py
@@ -1,3 +1,5 @@
+from pydantic import ConfigDict
+
 from fianchetto_tradebot.common_models.api.request import Request
 from fianchetto_tradebot.server.orders.managed_order_execution import ManagedExecution
 
@@ -5,3 +7,15 @@ from fianchetto_tradebot.server.orders.managed_order_execution import ManagedExe
 class CreateManagedExecutionRequest(Request):
     account_id: str
     managed_execution: ManagedExecution
+
+    def model_dump_json(self, **kwargs):
+        return super().model_dump_json(
+            **kwargs,
+            fallback=self._fallback
+        )
+
+    @staticmethod
+    def _fallback(obj):
+        if isinstance(obj, type):
+            return obj.__name__
+        raise TypeError(f"Cannot serialize object of type {type(obj).__name__}")
--- tests/moex/test_moex_req_res_serde.py
+++ tests/moex/test_moex_req_res_serde.py
@@ -3,6 +3,8 @@ import pytest
 from fianchetto_tradebot.common_models.brokerage.brokerage import Brokerage
 from fianchetto_tradebot.common_models.finance.amount import Amount
 from fianchetto_tradebot.common_models.finance.equity import Equity
+from fianchetto_tradebot.common_models.managed_executions.create_managed_execution_request import \
+    CreateManagedExecutionRequest
 from fianchetto_tradebot.common_models.order.action import Action
 from fianchetto_tradebot.common_models.order.expiry.good_until_cancelled import GoodUntilCancelled
 from fianchetto_tradebot.common_models.order.order import Order
@@ -38,7 +40,6 @@ def test_managed_execution_json_serde(managed_execution, account_id):
     assert reserialized_from_json.account_id == account_id
     assert reserialized_from_json.tactic == IncrementalPriceDeltaExecutionTactic
 
-
 def test_managed_execution_dict_serde(managed_execution: ManagedExecution, account_id: str):
     # serialize to json
     as_dict = managed_execution.model_dump()
@@ -49,3 +50,26 @@ def test_managed_execution_dict_serde(managed_execution: ManagedExecution, accou
     assert reserialized.account_id == account_id
     assert reserialized.tactic == IncrementalPriceDeltaExecutionTactic
 
+def test_create_managed_execution_request_json_serde(managed_execution, account_id):
+    create_managed_execution_request: CreateManagedExecutionRequest = CreateManagedExecutionRequest(account_id=account_id, managed_execution=managed_execution)
+
+    # serialize to json
+    as_json: str = create_managed_execution_request.model_dump_json()
+
+    # deserialize from json
+    reserialized_from_json: ManagedExecution = CreateManagedExecutionRequest.model_validate_json(as_json)
+
+    assert reserialized_from_json.account_id == account_id
+    assert reserialized_from_json.managed_execution.tactic == IncrementalPriceDeltaExecutionTactic
+
+def test_create_managed_execution_request_json_serde(managed_execution, account_id):
+    create_managed_execution_request: CreateManagedExecutionRequest = CreateManagedExecutionRequest(account_id=account_id, managed_execution=managed_execution)
+
+    # serialize to dict
+    as_dict: str = create_managed_execution_request.model_dump()
+
+    # deserialize from dict
+    reserialized_from_json: ManagedExecution = CreateManagedExecutionRequest.model_validate(as_dict)
+
+    assert reserialized_from_json.account_id == account_id
+    assert reserialized_from_json.managed_execution.tactic == IncrementalPriceDeltaExecutionTactic
