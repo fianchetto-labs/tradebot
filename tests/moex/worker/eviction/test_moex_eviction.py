244e29f0887e3aa4aa930926f1df36abebffaeaa
Aleks Shulman <aleksshulman@Alekss-MacBook-Pro.local>
2025-06-18 12:51:33 -0500

    FIA-125: Test outline for MOEX eviction

---
+++ tests/moex/worker/eviction/test_moex_eviction.py
@@ -0,0 +1,12 @@
+def test_order_cancelled_at_eod():
+    pass
+
+# TODO: Place into a separate class later
+def test_order_price_competitive():
+    pass
+
+def test_worker_stopped_after_moex_cancellation_request():
+    pass
+
+def test_evicted_worker_no_longer_in_thread_pool():
+    pass
